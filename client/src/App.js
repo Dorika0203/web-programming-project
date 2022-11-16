182aabbdfb7296ce78c4c5791eae9399e7d604f7
Dorika0203 <doyeol0203@gmail.com>
2022-11-16 18:46:04 +0900

    express - react initialize

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, {useEffect, useState} from 'react'
+
+function App() {
+
+  const [backendData, setBackendData] = useState([{}])
+
+  useEffect(() => {
+    fetch("/api")
+    .then(
+      response => response.json()
+    )
+    .then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, [])
+
+  console.log(backendData)
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use(express.static(
+    path.join(__dirname, '../client/build')
+))
+
+app.get("/api", (req, res) => {
+    res.json({"users": ["user1, user2, user3"]})
+})
+
+app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
+
+app.listen(5000, ()=> {console.log("Server started on port 5000")})
